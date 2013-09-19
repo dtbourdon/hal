4219a6b23218aeda95a10370f5a065b30019b9dc
Troy Bourdon <dbourdon@oreillyauto.com>
2013-09-19 13:04:53 -0700

    Rearaged some functions in hal.el and updated the README.

--- README.md
+++ README.md
@@ -1,4 +1,60 @@
-hal
-===
+# Synopsis
 
-An Emacs Lisp library to aid in JEE development.
+A simple Emacs library to aid in Java/JEE development. The basic workflow is
+to run a function in the library which returns a list of results in either the
+HAL-OUTPUT buffer or the HAL-TRANSCRIPT buffer. From there a function can be
+run on a selected item from the list. This will usually be opening a file but
+in the case of the HAL-TRANSCRIPT buffer it would be running a previous command.
+
+# Installation
+
+Please, note that the current version of `hal.el` requires find and grep
+
+## Manual
+
+Drop 'hal.el' in your load path and add the following to your .emacs file
+
+```lisp
+(defconst hal-workbench-dir "/your/development/workspace")
+(add-to-list 'load-path "~/.emacs.d/hal/")
+(require 'hal)
+```
+
+# Usage
+
+Command                                  | Description
+-----------------------------------------|----------------------------------------------------------------------------------------------
+<kbd>M-x hal-find-file</kbd>             | Find all files for the given search string and file type
+<kbd>M-x hal-find-in-file</kbd>          | Find all files in which the search string exists for the given file type
+<kbd>M-x hal-find-file-in-dir</kbd>      | Find all files for the search string and file type in the given directory
+<kbd>M-x hal-find-term-in-dir</kbd>      | Find all files that contain the search string for the given file type in the given directory
+<kbd>M-x hal-find-java</kbd>             | Convenience function that calls hal-find-file with file type of .java
+<kbd>M-x hal-find-groovy</kbd>           | Convenience function that calls hal-find-file with file type of .groovy
+<kbd>M-x hal-find-html</kbd>             | Convenience function that calls hal-find-file with file type of .html
+<kbd>M-x hal-find-xml</kbd>              | Convenience function that calls hal-find-file with file type of .xml
+<kbd>M-x hal-find-props</kbd>            | Convenience function that calls hal-find-file with file type of .properties
+<kbd>M-x hal-find-in-java</kbd>          | Convenience function that calls hal-find-in-file with file type of .java
+<kbd>M-x hal-find-in-groovy</kbd>        | Convenience function that calls hal-find-in-file with file type of .groovy
+<kbd>M-x hal-find-java-type</kbd>        | Find all java files which match the word under point
+<kbd>M-x hal-find-groovy-type</kbd>		 | Find all groovy files which match the word under point
+<kbd>M-x hal-find-java-imports</kbd>     | Find the java paths that match the search term and convert to import statemets 
+<kbd>M-x hal-open</kbd>                  | Open the file currently selected in the HAL-OUTPUT buffer
+<kbd>M-x hal-transcript</kbd>            | Display the HAL-TRANSCRIPT buffer with the list of previous commands for this session
+<kbd>M-x hal-run</kbd>                   | Run the currently selected command in the HAL-TRANSCRIPT buffer
+<kbd>M-x hal-copy-import</kbd>           | Copy the currently selected import line in the HAL-OUTPUT buffer 
+<kbd>M-x hal-yank-import</kbd>           | Yank the previously selected import to the line where point exists
+<kbd>M-x hal-b-kill</kbd>                | Kill the current buffer (eg HAL-OUTPUT or HAL-TRANSCRIPT)
+<kbd>M-x hal-w-kill</kbd>                | Kill the current window (eg HAL-OUTPUT or HAL-TRANSCRIPT)
+<kbd>M-x hal-bw-kill</kbd>               | Kill the current buffer and window (eg HAL-OUTPUT or HAL-TRANSCRIPT)
+
+
+# Known issues
+
+
+# Bugs & Improvements
+
+Bug reports and suggestions for improvements are always
+welcome. GitHub pull requests are even better! :-)
+
+Cheers,<br/>
+[dtbourdon](http://twitter.com/tbourdon)
--- hal.el
+++ hal.el
@@ -31,6 +31,7 @@
 (defvar hal-transcript-list "")
 (defvar hal-current-selected-import "")
 
+;; interactive functions
 (defun hal-find-file(search-term file-type)
   "Search for all files of type file-type that match the search-term and display results in the HAL-OUTPUT buffer."
   (interactive "sEnter Search Term: \nsEnter File Type: ")
@@ -55,54 +56,6 @@
   (hal-display-command-results
    (hal-fetch-command-results (hal-build-find-term-in-dir-command directory search-term (concat "*." file-type)))))
 
-(defun hal-display-command-results(command-results)
-  "Display the command results in the HAL-OUTPUT buffer. The command results are of a continuous string type."
-  (with-current-buffer (get-buffer-create hal-buffer-name)
-	(goto-char (point-min))
-	(insert command-results)
-	(goto-char (point-min)))
-  (pop-to-buffer hal-buffer-name))
-
-(defun hal-execute-command(command)
-  "Find all files returned from a properly formatted system command and display in the HAL-OUTPUT buffer."
-  (setq hal-transcript-list (concat hal-transcript-list command "\n"))
-  (hal-display-command-results (shell-command-to-string command)))
-
-(defun hal-fetch-command-results(command)
-  "Find all items returned from a properly formatted command and display in the HAL-OUTPUT buffer."
-  (setq hal-transcript-list (concat hal-transcript-list command "\n"))
-  (shell-command-to-string command))
-
-(defun hal-execute-transcript-command(command)
-  "Find all files returned from a properly formatted command and display in the HAL-OUTPUT buffer."
-  (setq hal-transcript-list (concat hal-transcript-list command "\n"))
-  (with-current-buffer (get-buffer-create hal-buffer-name)
-	(goto-char (point-min))
-	(insert (shell-command-to-string command))
-	(goto-char (point-min)))
-	(set-window-buffer (selected-window) hal-buffer-name))
-
-(defun hal-transcript()
-  "Show the HAL-TRANSCRIPT of previous commands for this session."
-  (interactive)
-  (with-current-buffer (get-buffer-create hal-transcript-name)
-	(goto-char (point-min))
-	(insert hal-transcript-list)
-	(goto-char (point-min)))
-	(pop-to-buffer hal-transcript-name))
-
-(defun hal-build-find-file-command(search-term file-type)
-  (concat "find " hal-workbench-dir " -type f -name '" file-type "'" " | grep " search-term " | grep -v target"))
-
-(defun hal-build-find-file-in-dir-command(directory search-term file-type)
-  (concat "find " directory " -type f -name '" file-type "'" " | grep " search-term " | grep -v target"))
-
-(defun hal-build-find-term-in-dir-command(directory search-term file-type)
-  (concat "grep -lrw " search-term " --include " file-type " " directory " | grep -v target"))
-
-(defun hal-build-find-in-file-command(search-term file-type)
-  (concat "grep -lrw " search-term " --include " file-type " " hal-workbench-dir " | grep -v target"))
-  
 (defun hal-find-groovy(search-term)
   "Search for all Groovy files that match the search-term and display in the HAL-OUTPUT buffer."
   (interactive "sEnter Search Term: ")
@@ -112,20 +65,6 @@
   "Search for all Java files that match the search-term and display in the HAL-OUTPUT buffer."
   (interactive "sEnter Search Term: ")
   (hal-find-file search-term "java"))
-
-(defun hal-find-java-paths(search-term)
-  "Return a list of all Java file paths that match the search-term."
-  (split-string (hal-fetch-command-results (hal-build-find-file-command search-term "*.java")) "\n"))
-
-(defun hal-find-java-type()
-  "Search for all Java files that match the word under point and display in the HAL-OUTPUT buffer."
-  (interactive)
-  (hal-find-file (thing-at-point 'word) "*.java"))
-
-(defun hal-find-in-java(search-term)
-  "Find all occurances of search-term in all Java files and display in the HAL-OUTPUT buffer."
-  (interactive "sEnter Search Term: ")
-  (hal-find-in-file search-term "*.java"))
 
 (defun hal-find-html(search-term)
   "Search for all HTML files that match the search-term and display in the HAL-OUTPUT buffer."
@@ -142,6 +81,26 @@
   (interactive "sEnter Search Term: ")
   (hal-find-file search-term "properties"))
 
+(defun hal-find-in-java(search-term)
+  "Find all occurances of search-term in all Java files and display in the HAL-OUTPUT buffer."
+  (interactive "sEnter Search Term: ")
+  (hal-find-in-file search-term "*.java"))
+
+(defun hal-find-in-groovy(search-term)
+  "Find all occurances of search-term in all Groovy files and display in the HAL-OUTPUT buffer."
+  (interactive "sEnter Search Term: ")
+  (hal-find-in-file search-term "*.groovy"))
+
+(defun hal-find-java-type()
+  "Search for all Java files that match the word under point and display in the HAL-OUTPUT buffer."
+  (interactive)
+  (hal-find-file (thing-at-point 'word) "*.java"))
+
+(defun hal-find-groovy-type()
+  "Search for all Groovy files that match the word under point and display in the HAL-OUTPUT buffer."
+  (interactive)
+  (hal-find-file (thing-at-point 'word) "*.groovy"))
+
 (defun hal-find-java-imports(search-term)
   "Find the java file paths that match the search-term and convert them to import statements"
   (interactive "sEnter Search Term: ")
@@ -152,30 +111,14 @@
 							  concat opackage))
   (hal-display-command-results command-results))
 
-(defun hal-java-paths-to-packages(java-paths)
-  "Given a list of java paths create a new list of packages that map to the paths."
-  (loop for path in java-paths
-		collect (hal-convert-java-path-to-package path)))
-
-(defun hal-convert-java-path-to-package(java-path)
-  "Given a path to a java file, trim off the leading part of the path before /com and replace the /'s with .'s"
-  (cond ((not (equal java-path "")) 
-		 (hal-prepend-import-to-java-import-string
-		  (hal-trim-java-from-import-string
-		   (replace-regexp-in-string "/" "." (substring java-path (search "com" java-path))))))))
-		
-(defun hal-trim-java-from-import-string(java-path-string)
-  "Given a dot delimeted path to a java path, trim off the trailing .java part"
-  (replace-regexp-in-string ".java" "" java-path-string))
-
-(defun hal-prepend-import-to-java-import-string(java-import-string)
-  "Given a dot delimeted path to a java path, prepend the import"
-  (concat "import " java-import-string ";"))
-
-(defun hal-concat-string-list(the-list separator)
-  "Iterate of the list and concatenate every element with the separator and the next element"
-  (loop for item in the-list
-		concat (item separator)))
+(defun hal-transcript()
+  "Show the HAL-TRANSCRIPT of previous commands for this session."
+  (interactive)
+  (with-current-buffer (get-buffer-create hal-transcript-name)
+	(goto-char (point-min))
+	(insert hal-transcript-list)
+	(goto-char (point-min)))
+	(pop-to-buffer hal-transcript-name))
 
 (defun hal-open()
   "Open the file currently selected in the HAL-OUTPUT buffer"
@@ -216,5 +159,74 @@
   "Yank the currently selected import to the current line where point exists"
   (interactive)
   (insert hal-current-selected-import))
+
+;; internal functions
+(defun hal-display-command-results(command-results)
+  "Display the command results in the HAL-OUTPUT buffer. The command results are of a continuous string type."
+  (with-current-buffer (get-buffer-create hal-buffer-name)
+	(goto-char (point-min))
+	(insert command-results)
+	(goto-char (point-min)))
+  (pop-to-buffer hal-buffer-name))
+
+(defun hal-execute-command(command)
+  "Find all files returned from a properly formatted system command and display in the HAL-OUTPUT buffer."
+  (setq hal-transcript-list (concat hal-transcript-list command "\n"))
+  (hal-display-command-results (shell-command-to-string command)))
+
+(defun hal-fetch-command-results(command)
+  "Find all items returned from a properly formatted command and display in the HAL-OUTPUT buffer."
+  (setq hal-transcript-list (concat hal-transcript-list command "\n"))
+  (shell-command-to-string command))
+
+(defun hal-execute-transcript-command(command)
+  "Find all files returned from a properly formatted command and display in the HAL-OUTPUT buffer."
+  (setq hal-transcript-list (concat hal-transcript-list command "\n"))
+  (with-current-buffer (get-buffer-create hal-buffer-name)
+	(goto-char (point-min))
+	(insert (shell-command-to-string command))
+	(goto-char (point-min)))
+	(set-window-buffer (selected-window) hal-buffer-name))
+
+(defun hal-build-find-file-command(search-term file-type)
+  (concat "find " hal-workbench-dir " -type f -name '" file-type "'" " | grep " search-term " | grep -v target"))
+
+(defun hal-build-find-file-in-dir-command(directory search-term file-type)
+  (concat "find " directory " -type f -name '" file-type "'" " | grep " search-term " | grep -v target"))
+
+(defun hal-build-find-term-in-dir-command(directory search-term file-type)
+  (concat "grep -lrw " search-term " --include " file-type " " directory " | grep -v target"))
+
+(defun hal-build-find-in-file-command(search-term file-type)
+  (concat "grep -lrw " search-term " --include " file-type " " hal-workbench-dir " | grep -v target"))
+
+(defun hal-find-java-paths(search-term)
+  "Return a list of all Java file paths that match the search-term."
+  (split-string (hal-fetch-command-results (hal-build-find-file-command search-term "*.java")) "\n"))
+
+(defun hal-java-paths-to-packages(java-paths)
+  "Given a list of java paths create a new list of packages that map to the paths."
+  (loop for path in java-paths
+		collect (hal-convert-java-path-to-package path)))
+
+(defun hal-convert-java-path-to-package(java-path)
+  "Given a path to a java file, trim off the leading part of the path before /com and replace the /'s with .'s"
+  (cond ((not (equal java-path "")) 
+		 (hal-prepend-import-to-java-import-string
+		  (hal-trim-java-from-import-string
+		   (replace-regexp-in-string "/" "." (substring java-path (search "com" java-path))))))))
+		
+(defun hal-trim-java-from-import-string(java-path-string)
+  "Given a dot delimeted path to a java path, trim off the trailing .java part"
+  (replace-regexp-in-string ".java" "" java-path-string))
+
+(defun hal-prepend-import-to-java-import-string(java-import-string)
+  "Given a dot delimeted path to a java path, prepend the import"
+  (concat "import " java-import-string ";"))
+
+(defun hal-concat-string-list(the-list separator)
+  "Iterate of the list and concatenate every element with the separator and the next element"
+  (loop for item in the-list
+		concat (item separator)))
 
 (provide 'hal)
